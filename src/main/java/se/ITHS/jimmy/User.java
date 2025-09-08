00f172aeaba1a96c3d664ce80991447e662afafb
Jimmy Grafström <jimmy.grafstrom@gmail.com>
2025-09-08 12:08:04 +0200

    Update User class. Added variables and get & set.

--- src/main/java/se/ITHS/jimmy/User.java
+++ src/main/java/se/ITHS/jimmy/User.java
@@ -2,4 +2,34 @@ package se.ITHS.jimmy;
 
 // Datamodell som hanterar info om anv
 public class User {
+    private String username;
+    private String password;
+    private int age;
+
+    // get & set for username
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+    // get & set for password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // get & set for age
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
