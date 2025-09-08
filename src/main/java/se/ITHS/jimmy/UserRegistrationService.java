d1a4ff208ca39899175477e5f00681072632a139
Jimmy Grafström <jimmy.grafstrom@gmail.com>
2025-09-08 13:22:27 +0200

    Update UserRegServ class. Added arraylist to save users and a method for adding new users.

--- src/main/java/se/ITHS/jimmy/UserRegistrationService.java
+++ src/main/java/se/ITHS/jimmy/UserRegistrationService.java
@@ -1,5 +1,14 @@
 package se.ITHS.jimmy;
 
+import java.util.ArrayList;
+
 // Hantering av logik bakom att skapa anv. Validering av lösenord och hantering av befintliga anv
 public class UserRegistrationService {
+    private ArrayList<User> savedUsers = new ArrayList<>();
+
+    public void addUser(User userToAdd) {
+        savedUsers.add(userToAdd);
+
+    }
+
 }
